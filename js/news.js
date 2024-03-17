6ffbe9a306b65080bdcfe5213ecb59be2141c914
Tim-Fender <timfenderua@gmail.com>
2024-03-17 15:17:24 +0200

    Update news.js

--- js/news.js
+++ js/news.js
@@ -3,18 +3,18 @@
 //   const shareButtons = document.querySelectorAll(".share-btn");
 //   shareButtons.forEach((button) => {
 //     button.addEventListener("click", () => {
-//       // Получаем уникальный идентификатор карточки новости
+//       // Отримання унікального ідентифікатора карточки новини
 //       const articleId = button.closest(".newsbox").dataset.articleId;
 
-//       // Формируем URL для шаринга на Facebook с учетом уникального идентификатора
+//       // Формування URL для sharing on Facebook з унікальним ідентифікатором
 //       const articleUrl = `https://timfender.github.io/777SEVENSMCC/news.html?id=${articleId}`;
 
-//       // Формируем ссылку для шаринга на Facebook
+//       // Формування посилання для sharing on Facebook
 //       const facebookShareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
 //         articleUrl
 //       )}`;
 
-//       // Открываем ссылку в новом окне
+//       // Відкриття посилання у новому вікні
 //       window.open(facebookShareUrl, "_blank");
 //     });
 //   });
